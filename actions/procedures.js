6cef5ba778c8d99caec0b2e0e7c24376dd9d3a95
Felipe Lima <pegasusyt1@gmail.com>
2024-08-22 15:26:19 -0300

    css design no index e adicionei a opção de ocultar os procedures ao clicar no ver novamente

--- actions/procedures.js
+++ actions/procedures.js
@@ -1,29 +1,33 @@
-// Função para gerar e exibir o relatório de produção
 async function gerarRelatorioProducao() {
   try {
-    const response = await fetch("http://localhost:3000/relatorio/producao");
-    if (!response.ok) {
-      throw new Error("Erro ao gerar o relatório de produção");
-    }
-
-    const relatorio = await response.json();
-
-    // Seleciona o div onde o relatório será exibido
     const relatorioDiv = document.getElementById("relatorioProducao");
-    relatorioDiv.innerHTML = "";
 
-    // Formata o relatório para exibição
-    relatorio.forEach((item) => {
-      const relatorioItem = document.createElement("div");
-      relatorioItem.classList.add(
-        "p-4",
-        "mb-4",
-        "bg-white",
-        "rounded",
-        "shadow-md"
-      );
+    // Verifica se o relatório já está sendo exibido
+    if (
+      relatorioDiv.style.display === "none" ||
+      relatorioDiv.innerHTML === ""
+    ) {
+      // Se não estiver, faz a requisição e exibe o relatório
+      const response = await fetch("http://localhost:3000/relatorio/producao");
+      if (!response.ok) {
+        throw new Error("Erro ao gerar o relatório de produção");
+      }
 
-      relatorioItem.innerHTML = `
+      const relatorio = await response.json();
+      relatorioDiv.innerHTML = "";
+
+      // Formata o relatório para exibição
+      relatorio.forEach((item) => {
+        const relatorioItem = document.createElement("div");
+        relatorioItem.classList.add(
+          "p-4",
+          "mb-4",
+          "bg-white",
+          "rounded",
+          "shadow-md"
+        );
+
+        relatorioItem.innerHTML = `
           <p><strong>Estufa:</strong> ${item.Estufa}</p>
           <p><strong>Lote:</strong> ${item.Lote}</p>
           <p><strong>Planta:</strong> ${item.Planta}</p>
@@ -31,8 +35,15 @@ async function gerarRelatorioProducao() {
           <p><strong>Média de Qualidade:</strong> ${item.Media_Qualidade}</p>
         `;
 
-      relatorioDiv.appendChild(relatorioItem);
-    });
+        relatorioDiv.appendChild(relatorioItem);
+      });
+
+      // Exibe o relatório
+      relatorioDiv.style.display = "block";
+    } else {
+      // Se já estiver sendo exibido, oculta o relatório
+      relatorioDiv.style.display = "none";
+    }
   } catch (error) {
     console.error("Erro:", error);
     alert(error.message);
